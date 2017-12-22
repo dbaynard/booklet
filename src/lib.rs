67974dbce50615121d9fc79df9baf656dff68f59
David Baynard <davidbaynard@gmail.com>
2017-12-22 21:04:30 +0000

    Add NonZero pseudo-newtype and attempt to quickcheck props

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,13 @@ extern crate num;
 #[macro_use]
 extern crate num_derive;
 
+use num::Unsigned;
+use num::Zero;
 use lopdf::Document;
 
 #[derive(Debug, FromPrimitive)]
 enum OnLeaf {
-    Zero,
+    Nil,
     One,
     Two,
     Three,
@@ -31,7 +33,7 @@ struct PageProps {
     blanks: OnLeaf,
 }
 
-fn mk_page_props(pages: u32) -> PageProps {
+fn mk_page_props(pages: &NonZero<u32>) -> PageProps {
     use num::FromPrimitive;
 
     // round up for the sheets of paper used
@@ -43,7 +45,7 @@ fn mk_page_props(pages: u32) -> PageProps {
     // The first page is the middle face, LHS
     let start_page = leaves * 2;
 
-    let blanks = FromPrimitive::from_u32(pages % 4)
+    let blanks = FromPrimitive::from_u32(pages.ex() % 4)
         .expect("This cannot fail");
 
     PageProps {
@@ -54,6 +56,46 @@ fn mk_page_props(pages: u32) -> PageProps {
     }
 }
 
-fn get_leaves(pages: &u32) -> u32 {
-    (pages - 1) / 4 + 1
+#[derive(Debug, PartialEq)]
+enum NonZero<T> {
+    NonZero(T),
+}
+
+fn mk_non_zero<T: Unsigned>(u: T) -> Option<NonZero<T>> {
+    if u.is_zero() {
+        None
+    } else {
+        Some(NonZero::NonZero(u))
+    }
+}
+
+impl<T> NonZero<T> {
+    fn ex(&self) -> &T {
+        use NonZero::NonZero;
+        match self {
+            &NonZero(ref t) => t,
+        }
+    }
+}
+
+fn get_leaves(pages: &NonZero<u32>) -> u32 {
+    (pages.ex() - 1) / 4 + 1
+}
+
+#[cfg(test)]
+mod test_get_leaves {
+    use super::*;
+
+    quickcheck! {
+        fn prop_exact_leaves_correct(x: u32) -> bool {
+            use NonZero::NonZero;
+            let y = match mk_non_zero(x / 4) {
+                Some(r) => r,
+                None => NonZero(1),
+            };
+            let z = get_leaves(&y);
+            println!("{:?}, {:?}", y.ex(), z);
+            *y.ex() == z
+        }
+    }
 }
