e3d4b86f967bb073bd2e2b1c5a5d8d8fd8eef8f4
David Baynard <davidbaynard@gmail.com>
2018-01-06 22:08:12 +0000

    Extract pages from input pdf

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,18 @@ extern crate num;
 extern crate num_derive;
 
 use lopdf::Document;
+use std::io;
+use std::io::ErrorKind::*;
 
 pub mod reorder;
 pub use reorder::*;
+
+pub fn reorder(infile: &str) -> Result<(), io::Error> {
+    let mut doc = Document::load(infile)?;
+    let mut in_pages = doc.get_pages();
+
+    let ps = NonZero::new(in_pages.len())
+        .ok_or(io::Error::new(InvalidInput, "Need nonzero document length"))?;
+
+    Ok(())
+}
