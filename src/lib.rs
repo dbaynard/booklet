3359186ff8a2630b42f3bcf9e4b434847278aa09
David Baynard <davidbaynard@gmail.com>
2018-01-07 02:24:35 +0000

    Rewrite page count (not order)

--- src/lib.rs
+++ src/lib.rs
@@ -20,14 +20,14 @@ pub use reorder::*;
 pub fn reorder(infile: &str) -> Result<(), io::Error> {
     let mut doc = Document::load(infile)?;
 
-    let pages = pages_location(&doc)
-        .ok_or(invalid("Couldn’t find ‘Pages’"))?;
-
     let in_pages = doc.get_pages();
-
-    let ps = NonZero::new(in_pages.len() as u32)
+    let pp = NonZero::new(in_pages.len() as u32)
+        .map(|x| PageProps::new(&x))
         .ok_or(nonzeroError())?;
-    let pp = PageProps::new(&ps);
+
+    // TODO Need to get typechecker to enforce this is called
+    rewrite_pages(&mut doc, &pp)?;
+
     let po = pp.print_order().map(|original_page| match original_page {
         None => {
             in_pages.get(&1)
@@ -42,6 +42,24 @@ pub fn reorder(infile: &str) -> Result<(), io::Error> {
                 .map(|x| *x)
         },
     });
+
+    Ok(())
+}
+
+fn rewrite_pages(doc: &mut Document, pp: &PageProps) -> Result<(), io::Error> {
+    let pages_loc = pages_location(&doc)
+        .ok_or(invalid("Couldn’t find ‘Pages’ location"))?;
+
+    let pages_dict = doc.get_object(pages_loc)
+        .and_then(Object::as_dict)
+        .ok_or(invalid("Couldn’t find ‘Pages’ dictionary"))?;
+
+    pages_dict.get("Count")
+        .iter().filter(
+            |x| x.as_i64().is_some()
+            ).next()
+        .map(|_| Object::Integer(pp.new_pages() as i64))
+        .ok_or(invalid("Couldn’t find ‘Count’ key"))?;
 
     Ok(())
 }
