de9ffdfab2365fd3712c97a103b56df166932b5e
David Baynard <davidbaynard@gmail.com>
2018-01-09 05:23:07 +0000

    Load options using structopt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 extern crate booklet;
 
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+use structopt::StructOpt;
+
 use booklet::*;
 use std::io;
 
@@ -12,4 +17,13 @@ fn main() {
 
 fn booklet() -> io::Result<()> {
     Ok(())
+}
+
+#[derive(StructOpt)]
+#[structopt(about="Rearrange pdf pages for booklet printing")]
+struct Opt {
+    /// Input file, if present (otherwise stdin)
+    input: Option<String>,
+    /// Output file, if present (otherwise stdout)
+    output: Option<String>,
 }
